f40a983f4fc2fc5fd5d113b41da1f8de7aadc387
TinhTran <55842203+TranNgocTinh0810@users.noreply.github.com>
2020-05-23 19:51:19 +0700

    Update Random.asm

--- Random.asm
+++ Random.asm
@@ -7,29 +7,40 @@
 .data 
       array: .word 0:100 # int array[100].save selected random number.
       sizecurent:.word    # size of array selected random number
+      
 .text main:
 .macro RanDom_int(%size)
-#khoi tao
-add $t7,$0,%size    #li $t7,size   #t7 save size 
-li $t1,0            # khoi tao t1
-#Check size <=100
- la $t1,sizecurent            
- li $t2,101            # t2 dung de so sanh 
- slt $t2,$t7,$t2
- blez $t2,resize                # Check $t2=0 <-> size >100
+
+        pushStack($t7)
+        pushStack($t0)
+	pushStack($t1)
+	pushStack($t2)
+	pushStack($a0)
+	pushStack($a1)
+	pushStack($a2)
+	pushStack($a3)
+
+        #khoi tao
+        add $t7,$0,%size    #li $t7,size   #t7 save size 
+         li $t1,0            # khoi tao t1
+        #Check size <=100
+        la $t1,sizecurent            
+        li $t2,101            # t2 dung de so sanh 
+        slt $t2,$t7,$t2
+        blez $t2,resize                # Check $t2=0 <-> size >100
  
- j RanDom
-resize:             # if (size) >100 -> size =100
-li $t7,100
+         j RanDom
+  resize:             # if (size) >100 -> size =100
+         li $t7,100
 
 RanDom:        
-la $a2,array          
-move $a1,$t7       #Range set from 0 to size(size max =100)
-li $v0,42          #generates random number and put it in $a0
-syscall
+          la $a2,array          
+          move $a1,$t7       #Range set from 0 to size(size max =100)
+          li $v0,42          #generates random number and put it in $a0
+          syscall
 
-#check
-li $t0,0 ##int count =0
+          #check
+          li $t0,0 ##int count =0
 check:
           lw $t1,sizecurent
           beq $t0,$t1,ExitLoop
@@ -42,24 +53,32 @@ check:
           addi $t0,$t0,1
           j check
 
-#a1 save ramdom number
+          #a1 save ramdom number
 ExitLoop:
-li $t2,4
-mult $t2,$t0
-mflo $t2
-sw $a0,($a2)   # save -> (a$2)
-sub $a2,$a2,$t2   # $a2 = $a2- 4*($t0)
+        li $t2,4
+        mult $t2,$t0
+        mflo $t2
+        sw $a0,($a2)   # save -> (a$2)
+        sub $a2,$a2,$t2   # $a2 = $a2- 4*($t0)
 
-#increase index
-addi $t1,$t1, 1
+        #increase index
+        addi $t1,$t1, 1
 
-sw $t1,sizecurent
-#Check full
-beq $t1,$t7,End
-move $v0,$a0
-j end_marco
+         sw $t1,sizecurent
+         #Check full
+         beq $t1,$t7,End
+         move $v0,$a0
+         j end_marco
 End:
-li $v0,10
-syscall
+         li $v0,10
+          syscall
 end_marco:
+        popStack($t7)
+        popStack($t0)
+	popStack($t1)
+	popStack($t2)
+	popStack($a0)
+	popStack($a1)
+	popStack($a2)
+	popStack($a3)
 .end_macro
